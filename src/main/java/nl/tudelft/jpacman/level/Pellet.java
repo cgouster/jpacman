3188470e538d46080ada823a1906eeb5e73d99ea
cgouster <74555595+cgouster@users.noreply.github.com>
2021-02-12 11:55:47 +0200

    Update Pellet.java

--- src/main/java/nl/tudelft/jpacman/level/Pellet.java
+++ src/main/java/nl/tudelft/jpacman/level/Pellet.java
@@ -42,4 +42,7 @@ public class Pellet extends Unit {
     public Sprite getSprite() {
         return image;
     }
+    public void collide(Unit collidedOn, PlayerCollisions playerCollisions) {
+		playerCollisions.pelletColliding(this, collidedOn);
+	}
 }
