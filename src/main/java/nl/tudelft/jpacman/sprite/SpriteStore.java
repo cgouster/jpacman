9f0f12d899f4417447356e6287a697c0b15455b9
cgouster <74555595+cgouster@users.noreply.github.com>
2021-02-16 12:14:32 +0200

    Update SpriteStore.java

--- src/main/java/nl/tudelft/jpacman/sprite/SpriteStore.java
+++ src/main/java/nl/tudelft/jpacman/sprite/SpriteStore.java
@@ -80,7 +80,7 @@ public class SpriteStore {
      *            Whether this sprite is a looping animation or not.
      * @return The animated sprite.
      */
-    public AnimatedSprite createAnimatedSprite(Sprite baseImage, int frames,
+    protected AnimatedSprite createAnimatedSprite(Sprite baseImage, int frames,
                                                int delay, boolean loop) {
         assert baseImage != null;
         assert frames > 0;
